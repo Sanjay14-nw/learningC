77832e0a8cfe3602555a17e1d5c3edbf2bdd7951
Sanjay14-nw <sb0295564@gmail.com>
2025-09-01 23:06:28 +0530

    Create calculater.c

---
+++ calculater.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+
+    printf("enter the first number:");
+    scanf("%d",&a);
+
+    printf("enter second number: ");
+    scanf("%d",&b);
+
+    int sum = a + b ;
+    printf("%d",sum);
+
+return 0;
+}
