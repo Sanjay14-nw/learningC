47893a50a5b2ea0962ae042169b25894604336ff
Sanjay14-nw <sb0295564@gmail.com>
2025-09-01 22:56:42 +0530

    Add files via upload

---
+++ hello_world.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+ {
+  
+    // int b;
+    
+    printf("enter the first number =");
+ 
+
+    // printf("enter the second number =");
+    // scanf("%d",&b);
+
+    // int c=a+b;
+    // printf("the sum of two digit =%d",c);
+
+}
