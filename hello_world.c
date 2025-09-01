1fe8c8a53159f621adbe56176ffdd278906595fa
Sanjay14-nw <sb0295564@gmail.com>
2025-09-01 23:07:30 +0530

    Update hello_world.c

--- hello_world.c
+++ hello_world.c
@@ -1,17 +1,6 @@
 #include<stdio.h>
 
 int main()
- {
-  
-    // int b;
-    
-    printf("enter the first number =");
- 
-
-    // printf("enter the second number =");
-    // scanf("%d",&b);
-
-    // int c=a+b;
-    // printf("the sum of two digit =%d",c);
-
+{   
+    printf("hello world");
 }
