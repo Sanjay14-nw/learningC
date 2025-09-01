8faff970e276b6af8a287edcffd4449614f7d1c2
Sanjay14-nw <sb0295564@gmail.com>
2025-09-01 23:09:21 +0530

    Create bodmas.c

---
+++ bodmas.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    printf("%d", 5 * 2 - 2 * 3 );
+
+    printf("%d", 5 * 2 / 2 * 3 );
+
+    printf("%d", 5 * (2 / 2)* 3 );
+
+    printf("%d", 5 + 2 / 2 * 3 );
+
+    return 0;
+
+}
