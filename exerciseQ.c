24c21be33b5453204d217583557125b2906b1f16
Sanjay14-nw <sb0295564@gmail.com>
2025-09-03 18:03:44 +0530

    Create exerciseQ.c

---
+++ exerciseQ.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+    int x,y,z;
+
+    printf("enter first number :");
+    scanf("%d",&x);
+
+    printf("enter second number :");
+    scanf("%d",&y);
+
+    printf("enter third number :");
+    scanf("%d",&z);
+
+    printf("average of three :%d", (x+y+z)/3);
+    return 0;
+}
