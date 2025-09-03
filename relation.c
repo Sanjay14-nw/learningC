b967e829c3fa4c0b7207aa9b523aa563902bb2f8
Sanjay14-nw <sb0295564@gmail.com>
2025-09-03 18:13:57 +0530

    Create relation.c

---
+++ relation.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main() {
+    // == (equal too)
+    printf("%d",4 == 4);
+    printf("%d",3 == 4);
+
+     //( >, >=) greater than
+    printf("%d",3 > 2);
+    printf("%d", 4 >= 4);
+
+    // ( != ) not equal too 
+    printf("%d ",4 == 4);
+     printf("%d ",4 != 4);
+}
