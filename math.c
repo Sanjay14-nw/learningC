ef8e802c9708293bafffaef62566f463b49b1925
Sanjay14-nw <sb0295564@gmail.com>
2025-09-03 18:06:39 +0530

    Create math.c

---
+++ math.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<math.h>
+ int main() {
+    int a = 3;
+    int b = 6;
+    a %= b;             //a /= b;       //a *= b;       //a -= b;          //a += b;
+    printf("%d",a);
+ }
